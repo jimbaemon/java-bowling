a6031a1cb658af160dd6932f68760562e24e2a25
jimbaemon <jimbae@kakao.com>
2022-11-09 00:32:08 +0900

    refactor: 첫번째 던지는것과 두번째 던지는것을 분리하기 위해 명명.

--- src/main/java/bowling/model/FrameStatus.java
+++ src/main/java/bowling/model/FrameStatus.java
@@ -1,16 +1,16 @@
 package bowling.model;
 
 public enum FrameStatus {
-	STRIKE,
-	SPARE,
-	MISS,
-	GUTTER,
-	NOT_AT_ALL;
+    STRIKE,
+    SPARE,
+    MISS,
+    GUTTER,
+    NOT_AT_ALL;
 
-	public static FrameStatus valueOf(final int firstBall){
-		if(firstBall == 10){
-			return STRIKE;
-		}
-		return NOT_AT_ALL;
-	}
+    public static FrameStatus firstShot(final int firstBall) {
+        if (firstBall == 10) {
+            return STRIKE;
+        }
+        return NOT_AT_ALL;
+    }
 }
--- src/test/java/bowling/model/FrameStatusTest.java
+++ src/test/java/bowling/model/FrameStatusTest.java
@@ -1,21 +1,21 @@
 package bowling.model;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FrameStatusTest {
 
-	@Test
-	void 첫번째_공에_10핀을_쓰러뜨리면_STRIKE(){
-		Assertions.assertThat(FrameStatus.valueOf(10)).isEqualTo(FrameStatus.STRIKE);
-	}
+    @Test
+    void 첫번째_공에_10핀을_쓰러뜨리면_STRIKE() {
+        assertThat(FrameStatus.firstShot(10)).isEqualTo(FrameStatus.STRIKE);
+    }
 
-	//TODO : 두번째 공에 10핀을 쓰러트리면 SPARE 이다.
+    //TODO : 두번째 공에 10핀을 쓰러트리면 SPARE 이다.
 
-	//TODO : 두번째 공에도 10핀을 모두 못 쓰러뜨리면 MISS 이다.
 
-	//TODO : 마지막 공에도 0핀을 쓰러뜨리면 GUTTER 이다.
+    //TODO : 두번째 공에도 10핀을 모두 못 쓰러뜨리면 MISS 이다.
+
+    //TODO : 마지막 공에도 0핀을 쓰러뜨리면 GUTTER 이다.
 
 }
