c1bc818833e169d94580a9295b2271111dcf42bb
jimbaemon <jimbae@kakao.com>
2022-11-09 01:32:37 +0900

    docs: 기능 재명세.

--- src/test/java/bowling/model/FrameTest.java
+++ src/test/java/bowling/model/FrameTest.java
@@ -11,4 +11,8 @@ class FrameTest {
         Frame frame = new Frame(10);
         assertThat(frame.result()).isEqualTo(FrameStatus.STRIKE);
     }
+
+    //TODO: 투구 결과가 Strike 인데 두번째 점수 입력시 에러 발생
+
+    //TODO: 세번째 점수 입력시 에러 발생
 }
