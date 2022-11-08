ab0137aebb27f0313433cf227a74f93f32c61c47
jimbaemon <jimbae@kakao.com>
2022-11-09 01:44:30 +0900

    feat: 점수 입력 3번시 에러 발생.

--- src/main/java/bowling/model/Frame.java
+++ src/main/java/bowling/model/Frame.java
@@ -15,12 +15,19 @@ public class Frame {
 
 
     public FrameStatus result() {
-        return FrameStatus.firstShot(scores.get(0));
+        if (scores.size() == 1) {
+            return FrameStatus.firstShot(scores.get(0));
+        }
+        return FrameStatus.secondShot(scores.get(0), scores.get(1));
     }
 
     public void addScore(int score) {
         if (result() == FrameStatus.STRIKE) {
             throw new IllegalStateException("스트라이크 상태에서 점수를 추가 할 수 없습니다.");
         }
+        if (scores.size() == 2) {
+            throw new IllegalArgumentException("점수 입력은 최대 2회만 가능합니다.");
+        }
+        scores.add(score);
     }
 }
--- src/test/java/bowling/model/FrameTest.java
+++ src/test/java/bowling/model/FrameTest.java
@@ -1,8 +1,8 @@
 package bowling.model;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class FrameTest {
@@ -12,14 +12,21 @@ class FrameTest {
         Frame frame = new Frame(10);
         assertThat(frame.result()).isEqualTo(FrameStatus.STRIKE);
     }
-    
+
     @Test
     void STRIKE_에서_두번쨰_점수_입력시_에러_발생() {
         Frame frame = new Frame(10);
-        Assertions.assertThatThrownBy(() -> frame.addScore(0))
+        assertThatThrownBy(() -> frame.addScore(0))
             .isInstanceOf(IllegalStateException.class)
             .hasMessage("스트라이크 상태에서 점수를 추가 할 수 없습니다.");
     }
 
-    //TODO: 세번째 점수 입력시 에러 발생
+    @Test
+    void 세번째_점수_입력시_에러_발생() {
+        Frame frame = new Frame(8);
+        frame.addScore(2);
+        assertThatThrownBy(() -> frame.addScore(2))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("점수 입력은 최대 2회만 가능합니다.");
+    }
 }
