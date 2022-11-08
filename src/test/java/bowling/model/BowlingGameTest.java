b0fcc9527abe0f8fb1afc63ec63325f5261b1cf1
jimbaemon <jimbae@kakao.com>
2022-11-08 21:19:43 +0900

    docs: 1차 기능 정의.

---
+++ src/test/java/bowling/model/BowlingGameTest.java
@@ -0,0 +1,13 @@
+package bowling.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+
+	//TODO: 볼링 게임은 플레이어가 있어야 시작한다.
+
+	//TODO: 볼링게임은 10프레임까지 진행된 후 종료 된다.
+
+	//TODO: 볼링게임 결과를 반환한다.
+
+}
---
+++ src/test/java/bowling/model/FrameStatusTest.java
@@ -0,0 +1,15 @@
+package bowling.model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FrameStatusTest {
+
+	//TODO: 첫번째 공에 10핀을 쓰러뜨리면 STRIKE 이다.
+
+	//TODO : 두번째 공에 10핀을 쓰러트리면 SPARE 이다.
+
+	//TODO : 두번째 공에도 10핀을 모두 못 쓰러뜨리면 MISS 이다.
+
+	//TODO : 마지막 공에도 0핀을 쓰러뜨리면 GUTTER 이다.
+
+}
